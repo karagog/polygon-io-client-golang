089f68d6cb7ed03143bb4507a3a3604cc08f0f9a
George Karagoulis <karagog@gmail.com>
2021-09-03 16:47:05 -0700

    Added a working example that queries the tickers API.

--- client/client.go
+++ client/client.go
@@ -1,6 +1,18 @@
 // Package client is a client object for accessing Polygon-IO.
 package client
 
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+const (
+	addr = "https://api.polygon.io"
+)
+
 // Client is a client object. Create with New().
 type Client struct {
 	apiKey string
@@ -11,4 +23,53 @@ func New(apiKey string) *Client {
 	return &Client{
 		apiKey: apiKey,
 	}
+}
+
+type TickersOptions struct {
+	Market  string // stocks, crypto, fx
+	Symbols []string
+	Search  string
+}
+
+const tickerPageLimit = 50
+
+func (c *Client) Tickers(ctx context.Context, opt *TickersOptions) ([]byte, error) {
+	r, err := http.NewRequestWithContext(ctx, "GET",
+		fmt.Sprintf("%s/v3/reference/tickers"+
+			"?apiKey=%s"+
+			"&market=%s"+
+			"&limit=%d"+
+			"&ticker=%s"+
+			"&search=%s"+
+			"&sort=ticker",
+			addr,
+			c.apiKey,
+			opt.Market,
+			tickerPageLimit,
+			strings.Join(opt.Symbols, ","),
+			opt.Search,
+		), nil)
+	if err != nil {
+		return nil, err
+	}
+	return doRequest(r)
+}
+
+// Runs the request against the server and returns the raw response JSON.
+func doRequest(r *http.Request) ([]byte, error) {
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status (%d): %s", resp.StatusCode, body)
+	}
+	return body, nil
 }
---
+++ client/testcli/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/karagog/polygon-io-client-golang/client"
+)
+
+// Set the environment variable like so:
+// $ export POLYGON_IO_API_KEY=<abcd>
+const apiKeyEnvVar = "POLYGON_IO_API_KEY"
+
+var ctx = context.Background()
+
+func main() {
+	tickersCmd := flag.NewFlagSet("tickers", flag.ExitOnError)
+	tickersSymbols := tickersCmd.String("symbols", "", "Comma-separated symbols to query")
+	tickersCmd.Usage = func() {
+		fmt.Printf("Usage of the tickers command:\n\n")
+		fmt.Println("\ttestcli tickers --symbols={comma-separated-list}")
+	}
+
+	// Get the API key from the environment.
+	apiKey := os.Getenv(apiKeyEnvVar)
+	if apiKey == "" {
+		panic(fmt.Sprintf("API key not specified in environment variable: %s", apiKeyEnvVar))
+	}
+	c := client.New(apiKey)
+
+	flag.Usage = func() {
+		fmt.Printf("Usage of %s:\n\n", os.Args[0])
+		fmt.Println("\ttestcli {command} [-h]")
+		fmt.Println()
+		fmt.Println("Run with -h to get usage details for each command")
+	}
+	flag.Parse()
+	flag.Set("alsologtostderr", "true")
+
+	if len(os.Args) < 2 {
+		panic("You must specify a command")
+	}
+
+	var response []byte
+	var err error
+	cmd := os.Args[1]
+	switch cmd {
+	case "tickers":
+		tickersCmd.Parse(os.Args[2:])
+		response, err = c.Tickers(ctx, &client.TickersOptions{
+			Symbols: strings.Split(*tickersSymbols, ","),
+		})
+	default:
+		panic(fmt.Sprintf("Unknown command: %s", cmd))
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	// Show the raw response, with indentations to make it readable.
+	var indented bytes.Buffer
+	if err := json.Indent(&indented, response, "", "\t"); err != nil {
+		panic(err)
+	}
+	fmt.Println("Raw json response: \n", indented.String())
+}
